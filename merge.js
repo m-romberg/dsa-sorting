549dbbff0ea576f81a672484e2c7fa32809062c8
sarah <sarahgraup@g.ucla.edu>
2023-06-02 11:40:23 -0700

    added merge function

--- merge.js
+++ merge.js
@@ -1,4 +1,31 @@
-function merge() {}
+function merge(arr1, arr2) {
+    let results = [];
+    let i=0;
+    let j=0;
+
+    while(i<arr1.length && j<arr2.length){
+        if(arr1[i] <= arr2[j]){
+            results.push(arr1[i]);
+            i++;
+
+        }else{
+            results.push(arr2[j]);
+            j++;
+
+        }
+    }
+    while (i<arr1.length){
+        results.push(arr1[i]);
+        i++;
+    }
+    while (j<arr2.length){
+        results.push(arr2[j]);
+        j++;
+    }
+
+    return results;
+
+}
 
 function mergeSort() {}
 
